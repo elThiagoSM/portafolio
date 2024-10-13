1dbc8aa47b91c64bab4e543e5cee0a0bff1d8dd6
Thiago Silveira Machado <102184394+elThiagoSM@users.noreply.github.com>
2024-10-13 12:35:32 -0300

    install-gh-pages-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Services from './pages/Services';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
 
 function App() {
   return (
